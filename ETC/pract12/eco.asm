8ab06542781b2b53ee57f57c44fc28a10568cdcb
jaime999 <martinezsanchezjaime@gmail.com>
2021-02-28 17:55:58 +0100

    Actividad 3 de la práctica 12.

---
+++ ETC/pract12/eco.asm
@@ -0,0 +1,72 @@
+### PROGRAMA BASE PRACTICAS DE ENTRADA/SALIDA MIPS R2000
+
+#-------------------------------------------------#
+#
+#  PRÁCTICA 12: SINCRONIZACIÓN POR PRUEBA DE ESTADO
+# 
+#-------------------------------------------------#
+
+# ACTIVIDAD 3:  Completar las funciones:
+#    char getchar() - obtiene el carácter del teclado
+#    void putchar(char c) - imprime un carácter por la consola
+
+# Segmento de datos
+
+	.data		
+
+#-------------------------------------------------#
+
+# Segmento de código ("text")
+	.text
+    	.globl __start	
+
+
+
+__start:			
+
+	li $a0, 'P'		# 
+	jal putchar		# putchar('P')
+	li $a0, '1'		# 
+	jal putchar		# putchar('1')
+	li $a0, '2'		# 
+	jal putchar		# putchar('2')
+	li $a0, 13		# carácter de retorno ('\n')
+	jal putchar		# putchar('\n')
+	
+bucle:
+	jal getchar		# $v0 = getchar()
+	move $a0, $v0		#
+	li $t0, 0x1b      	# detecto ESC (0x1b = 27)
+	beq $a0, $t0, fin
+	jal putchar		# putchar($a0)
+	b bucle
+fin:	
+	li $v0, 10
+	syscall			# exit
+	
+	
+	
+getchar:			# $v0 = getchar()
+	la $t0, 0xffff0000
+	
+esperagetchar:	lw $t1,0($t0)
+				andi $t1,$t1,1
+				beqz $t1,esperagetchar
+				lw $v0, 4($t0)
+				li $v1, 12
+				syscall
+				move $v0, $a0
+
+				jr $ra
+
+
+putchar:			# putchar($a0)
+	la $t0, 0xffff0008
+	
+esperaputchar: 	lw $t1,0($t0)
+				andi $t1,$t1,1
+				beqz $t1,esperaputchar
+				sw $a0, 4($t0)
+
+
+				jr $ra
